3d4e8c0db24372d054007bc21d5bc9be89ec87c8
chiluhan <1248754026@qq.com>
2020-09-15 20:24:59 +0800

    09-15

---
+++ 第一阶段/day02/exercise02.py
@@ -0,0 +1,9 @@
+"""
+    判断英文句子成分：I kiss you
+    体会：人机交互
+"""
+subject = input("输入 I kiss you 的主语：")
+predicate = input("输入 I kiss you 的谓语：")
+object = input("输入 I kiss you 的宾语：")
+print("您输入的是：")
+print("主语：" + subject + ",谓语：" + predicate + ",宾语：" + object)
